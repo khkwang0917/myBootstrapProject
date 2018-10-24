a620606993a0aacd029ca1ad7126c2c99705311e
Kwang Kian Hao  (NCS) <kianhao.kwang@ncs.com.sg>
2018-10-24 17:50:44 +0800

    Added animation when scrolling from anchor as well as back to top button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,55 @@ $( document ).ready(function() {
   /* Control the gallries */
   $('#galleries').carousel();
 
+
+  $('body').append('<div id="toTop" class="btn btn-info"><span class="glyphicon glyphicon-chevron-up"></span>Top</div>');
+  $(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#toTop').fadeIn();
+		} else {
+			$('#toTop').fadeOut();
+		}
+	});
+  $('#toTop').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 200);
+      return false;
+  });
+
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
 });
